ba95d85a576b8d309e128362c94974821fba8f6d
Rajat Gupta <guptarajat234@gmail.com>
2024-07-12 20:26:36 +0530

    panoramic viewer added

---
+++ src/app/panoramic.js
@@ -0,0 +1,47 @@
+"use client"
+import React from "react";
+import ReactPannellum, { getConfig } from "react-pannellum";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExpand } from "@fortawesome/free-solid-svg-icons";
+const Panoramic = () => {
+  const click = () => {
+    console.log(getConfig("firstScene"));
+  };
+
+  const config = {
+    autoRotate: -2,
+    autoLoad: true,
+  };
+  const style ={
+    width: '90vw' ,
+    height: "80vh",
+    background: "#000000"
+
+  }
+  return (
+    <div className="w-full flex flex-col items-center">
+        <p className="sm:mb-4  mb-2 text-center text-gray-700">
+        Swipe on mobile devices or click and drag on desktops to change the view.
+      </p>
+      <ReactPannellum
+        id="1"
+        sceneId="firstScene"
+        imageSource="/1.jpg"
+        config={config}
+        style={style}
+      />
+        <p className="sm:mt-4 mt-2 text-center text-gray-700 flex sm:flex-row flex-col items-center">
+        For the best experience, click on 
+       
+       <p> <span className="inline-flex items-center m-2">
+          <FontAwesomeIcon icon={faExpand} />
+        </span>
+        full-screen icon for full width and maximum view.
+        </p>
+      </p>
+      <div onClick={click}>Click me</div>
+    </div>
+  );
+};
+
+export default Panoramic;
