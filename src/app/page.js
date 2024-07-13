eeb17d14fed8455f8d51ed466bf5a7ad81284c7a
Rajat Gupta <guptarajat234@gmail.com>
2024-07-13 17:41:48 +0530

    logo size fix

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="App w-full flex flex-col items-center font-mono">
       <div className="flex sm:flex-row flex-col sm:items-end items-center gap-x-6 sm:mt-4 mt-2"> 
-      <img src={'/logo.png'} className="w-[40vw] sm:w-[20vw]"/>
+      <img src={'/logo.png'} className="w-[40vw] sm:w-[16vw]"/>
     <h1 className="font-mono"> The 360° Vision Explorer</h1>
     </div>
     <Panoramic />
