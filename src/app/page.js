00bb1aaf95b0b2b18408b3e6ff260955588db115
Rajat Gupta <guptarajat234@gmail.com>
2024-07-13 17:43:49 +0530

    margin fix

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,8 @@ import Panoramic from "./panoramic";
 export default function Home() {
   return (
     <div className="App w-full flex flex-col items-center font-mono">
-      <div className="flex sm:flex-row flex-col sm:items-end items-center gap-x-6 sm:mt-4 mt-2"> 
-      <img src={'/logo.png'} className="w-[40vw] sm:w-[16vw]"/>
+      <div className="flex sm:flex-row flex-col sm:items-end items-center gap-x-6 sm:mt-3 mt-2"> 
+      <img src={'/logo.png'} className="w-[40vw] sm:w-[15vw]"/>
     <h1 className="font-mono"> The 360° Vision Explorer</h1>
     </div>
     <Panoramic />
