5575abbcf2cef008d3b0b479c86441bf7c2e9150
Rajat Gupta <guptarajat234@gmail.com>
2024-07-14 07:55:12 +0530

    hide after 5 secs

--- src/app/panoramic.js
+++ src/app/panoramic.js
@@ -1,9 +1,20 @@
-"use client"
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 import ReactPannellum, { getConfig } from "react-pannellum";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faExpand } from "@fortawesome/free-solid-svg-icons";
+
 const Panoramic = () => {
+  const [showHint, setShowHint] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowHint(false);
+    }, 5000); // 5 seconds
+
+    return () => clearTimeout(timer); // Cleanup the timer
+  }, []);
+
   const click = () => {
     console.log(getConfig("firstScene"));
   };
@@ -12,31 +23,35 @@ const Panoramic = () => {
     autoRotate: -2,
     autoLoad: true,
   };
-  const style ={
-    width: '90vw' ,
+
+  const style = {
+    width: '90vw',
     height: "80vh",
     background: "#000000"
+  };
 
-  }
   return (
     <div className="w-full flex flex-col items-center">
+      {showHint && (
         <p className="sm:my-2 text-sm my-1 italic text-center text-gray-700 font-light">
-        Swipe over the image on mobile devices, or click and drag on desktops to change the view.
-      </p>
+          Swipe over the image on mobile devices, or click and drag on desktops to change the view.
+        </p>
+      )}
       <ReactPannellum
         id="1"
         sceneId="firstScene"
         imageSource="/2.jpg"
         config={config}
         style={style}
+        onPanoramaLoaded={() => console.log('cs')}
       />
-        <p className="sm:mt-4 mt-2 italic text-center text-gray-700 flex sm:flex-row flex-col items-center">
+      <p className="sm:mt-4 mt-2 italic text-center text-gray-700 flex sm:flex-row flex-col items-center">
         For the best experience, click on 
-       
-       <p> <span className="inline-flex items-center m-2">
-          <FontAwesomeIcon icon={faExpand} />
-        </span>
-        full-screen icon for full width and maximum view.
+        <p>
+          <span className="inline-flex items-center m-2">
+            <FontAwesomeIcon icon={faExpand} />
+          </span>
+          full-screen icon for full width and maximum view.
         </p>
       </p>
       <div onClick={click}>Click me</div>
